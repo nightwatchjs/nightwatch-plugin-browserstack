a048a7e4b30ee51a7736e08a579287ff24e5caa1
Binayak Ghosh <ghoshbinayak@gmail.com>
2023-06-29 21:10:35 +0530

    refactor

--- test/src/test-observability/processTestRunData.js
+++ test/src/test-observability/processTestRunData.js
@@ -2,39 +2,37 @@ const TestObservability = require('../../../src/testObservability');
 const sinon = require('sinon');
 
 describe('TestObservability - processTestRunData', function () {
-  let testObservability; let sandbox; let eventData; let sectionName; let testFileReport; let hookIds; let sendTestRunEventStub;
-
   beforeEach(() => {
-    sandbox = sinon.createSandbox();
-    testObservability = new TestObservability();
+    this.sandbox = sinon.createSandbox();
+    this.testObservability = new TestObservability();
 
-    eventData = {commands: []};
-    sectionName = 'testSection';
-    testFileReport = {};
-    hookIds = [];
-    sendTestRunEventStub = sandbox.stub(testObservability, 'sendTestRunEvent').resolves();
+    this.eventData = {commands: []};
+    this.sectionName = 'testSection';
+    this.testFileReport = {};
+    this.hookIds = [];
+    this.sendTestRunEventStub = this.sandbox.stub(this.testObservability, 'sendTestRunEvent').resolves();
   });
 
   afterEach(() => {
-    sandbox.restore();
+    this.sandbox.restore();
   });
 
-  it('should send test run events', async function () {
-    await testObservability.processTestRunData(eventData, sectionName, testFileReport, hookIds);
-    sinon.assert.calledTwice(sendTestRunEventStub);
-    sinon.assert.calledWith(sendTestRunEventStub.firstCall, eventData, testFileReport, 'TestRunStarted', sinon.match.string, null, sectionName, hookIds);
-    sinon.assert.calledWith(sendTestRunEventStub.secondCall, eventData, testFileReport, 'TestRunFinished', sinon.match.string, null, sectionName, hookIds);
+  it('should send test run events', async () => {
+    await this.testObservability.processTestRunData(this.eventData, this.sectionName, this.testFileReport, this.hookIds);
+    sinon.assert.calledTwice(this.sendTestRunEventStub);
+    sinon.assert.calledWith(this.sendTestRunEventStub.firstCall, this.eventData, this.testFileReport, 'TestRunStarted', sinon.match.string, null, this.sectionName, this.hookIds);
+    sinon.assert.calledWith(this.sendTestRunEventStub.secondCall, this.eventData, this.testFileReport, 'TestRunFinished', sinon.match.string, null, this.sectionName, this.hookIds);
   });
 
   it('should create screenshot log events', async () => {
-    eventData = {
+    this.eventData = {
       commands: [
         {name: 'saveScreenshot', args: ['path/to/screenshot.png'], startTime: 'start_time'}
       ]
     };
-    const createScreenshotLogEventStub = sandbox.stub(testObservability, 'createScreenshotLogEvent');
+    const createScreenshotLogEventStub = this.sandbox.stub(this.testObservability, 'createScreenshotLogEvent');
     process.env.BS_TESTOPS_ALLOW_SCREENSHOTS = 'true';
-    await testObservability.processTestRunData(eventData, sectionName, testFileReport, hookIds);
+    await this.testObservability.processTestRunData(this.eventData, this.sectionName, this.testFileReport, this.hookIds);
     process.env.BS_TESTOPS_ALLOW_SCREENSHOTS = 'false';
     sinon.assert.calledOnceWithExactly(createScreenshotLogEventStub, sinon.match.string, 'path/to/screenshot.png', 'start_time');
   });
