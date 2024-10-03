5a47d312199f0e56d2fa32fd98f98bf1aeec342a
rev-doshi <riya.d@browserstack.com>
2024-10-03 15:02:12 +0530

    ensuring screesnhot reporting on observability + spell check

--- nightwatch/globals.js
+++ nightwatch/globals.js
@@ -295,6 +295,15 @@ module.exports = {
     try {
       testObservability.configure(settings);
       if (helper.isTestObservabilitySession()) {
+        if (settings.reporter_options) {
+          if (settings.reporter_options['save_command_result_value'] != true){
+            settings.reporter_options['save_command_result_value'] = true
+          }
+        } else {
+          settings.reporter_options = {
+            save_command_result_value: true
+          }
+        }
         if (helper.isCucumberTestSuite(settings)) {
           cucumberPatcher();
           process.env.CUCUMBER_SUITE = 'true';
