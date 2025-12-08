a7f66fd2bf6b05cb9d47317b5d2d7ea9f2f9f974
Bhargavi-BS <bhargavi@browserstack.com>
2025-12-08 19:00:40 +0530

    fix for the timeout issue

--- nightwatch/globals.js
+++ nightwatch/globals.js
@@ -541,6 +541,8 @@ module.exports = {
   },
 
   async afterChildProcess() {
+
+    await helper.shutDownRequestHandler();
     if (testEventPromises.length > 0) {
       await Promise.all(testEventPromises);
       testEventPromises.length = 0; // Clear the array
